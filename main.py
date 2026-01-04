daad93462edd4b316f214187d5445611e44bede3
MRizkiii <rizkigaming048@gmail.com>
2026-01-04 18:42:05 +0700

    WIP: ssh auth.log parsing without fixed indexes

--- main.py
+++ main.py
@@ -33,35 +33,109 @@ def data(bulan, tanggal, jam, user, ip, port):
         "port" : port
     }
 
-#DATA LOGIN
+sample = []
+newBulanAcc = []
+newTanggalAcc = []
+newJamAcc = []
+
+newBulanFai = []
+newTanggalFai = []
+newJamFai = []
+
+newJamInv = []
+newBulanInv = []
+newTanggalInv = []
+
+
+def time(inti, bulan, tanggal, jam):
+    for y in range(len(inti)):
+        sample.append(list(filter(None, inti[y].split())))
+    for x in sample:
+        bulan.append(x[0])
+        tanggal.append(x[1])
+        jam.append(x[2])
+
+    return bulan, tanggal, jam
+
+time(berhasil, newBulanAcc, newTanggalAcc, newJamAcc)
+time(gagal, newBulanFai, newTanggalFai, newJamFai)
+time(invalid, newBulanInv, newTanggalInv, newJamInv )
+
+def dataData(inti,newUserList, newIpList, newPortList):
+    for x in inti:
+        pos = 0
+        while True:
+            user = x.find('for ', pos)
+            ip = x.find('from ', pos)
+            port = x.find('port ', pos)
+
+            if user == -1:
+                break
+        
+
+            awalID = user + 4
+            akhirID = x.find(' ', awalID)
+
+            awalIP = ip + 5
+            akhirIP = x.find(' ', awalIP)
+
+            awalPort = port + 5
+            akhirPort = x.find(" ", awalPort)
+
+        
+            if akhirID == -1:
+                break
+            
+            newUserList.append(x[awalID:akhirID])
+            newIpList.append(x[awalIP:akhirIP])
+            newPortList.append(x[awalPort:akhirPort])
+            pos = akhirID + 1
+
+    return user, ip, port
+
+
+newUserAcc = []
+newIPAcc = []
+newPortAcc = []
+
+newUserFai = []
+newIPFai = []
+newPortFai = []
+
+newUserInv = []
+newIPInv = []
+newPortInv = []
+
+dataData(berhasil, newUserAcc, newIPAcc, newPortAcc )
+dataData(gagal, newUserFai, newIPFai, newPortFai)
+dataData(invalid, newUserInv, newIPInv, newPortInv)
+
+
 dataBerhasilLog = []
-for line in berhasil:
-    x = line.split()
-    dataBerhasilLog.append(data(x[0], x[1], x[2], x[8], x[10], x[12]))
+dataGagalLog = []
+dataInvalidLog = []
 
-# print(dataBerhasilLog)
-# for x in dataBerhasilLog:
-#     x = x["user"]
-#     print(x)
+def fullData(container, Len, bulan, tanggal, jam, user, ip, port):
+    for i in range(len(Len)):
+        container.append(data(bulan[i], tanggal[i], jam[i], user[i], ip[i], port[i]))
+        
+    return bulan, tanggal, jam, user, ip, port
 
-#USER LOGIN
+fullData(dataBerhasilLog, newUserAcc, newBulanAcc, newTanggalAcc, newJamAcc, newUserAcc, newIPAcc, newPortAcc)
+fullData(dataGagalLog, newUserFai, newBulanFai, newTanggalFai, newJamFai, newUserFai, newIPFai, newPortFai)
+fullData(dataInvalidLog, newUserInv, newBulanInv, newTanggalInv, newJamInv, newUserInv, newIPInv, newPortInv)
+
+
 userLogin = []
 userLoginTotal = {}
 for x in dataBerhasilLog:
     x = x["user"]
     userLogin.append(x)
-for x in userLogin:
-    userLoginTotal[x] = userLoginTotal.get(x, 0) + 1
+# for x in userLogin:
+    # userLoginTotal[x] = userLoginTotal.get(x, 0) + 1
 
 
 #GAGAL DATA
-dataGagalLog = []
-for x in gagal:
-    x = x.split()
-    if x[8] != 'times:':
-        dataGagalLog.append(data(x[0], x[1], x[2], x[8], x[10], x[12]))
-
-
 # for x in dataGagalLog:
 #      x = x['user']
 #      print(x)
@@ -96,12 +170,8 @@ for x in newInvalid:
 
 # print(realInvalid)
 
-dataInvalidLog = []
-for x in realInvalid:
-    # print(x[10])
-    dataInvalidLog.append(data(x[0], x[1], x[2], x[10], x[12], x[14]))
 
-# print(dataInvalidLog)
+# InvalidLog)
 
 # USER INVALID
 userInvalid = []
@@ -113,11 +183,11 @@ for x in dataInvalidLog:
 for x in userInvalid:
     userInvalidTotal[x] = userInvalidTotal.get(x, 0) + 1
 
-print(userLoginTotal)
-print("////////////////////////////////////////")
-print(userGagalTotal)
-print("////////////////////////////////////////")
-print(userInvalidTotal)
+# print(userLoginTotal)
+# print("////////////////////////////////////////")
+# print(userGagalTotal)
+# print("////////////////////////////////////////")
+# print(userInvalidTotal)
 
 
 

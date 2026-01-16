ff4acbb0e75dff2143b345ac2ee144083a807080
MRizkiii <rizkigaming048@gmail.com>
2026-01-16 23:30:47 +0700

    improve code be more reusable

--- main.py
+++ main.py
@@ -26,41 +26,44 @@ def printHasil(IP, USER, JAM):
 
  
 ##LOGIC
-fullData = []
-with open("auth.log", "r") as f:
-    for x in f:
-        if log(x) and is_valid_login_line(x):
-            data = parsing(x)
-            fullData.append(data)
+def main():
+    fullData = []
+    with open("auth.log", "r") as f:
+        for x in f:
+            if log(x) and is_valid_login_line(x):
+                data = parsing(x)
+                fullData.append(data)
 
 
-acceptedLog = []
-failedLog = []
-invalidUserLog = []
+    acceptedLog = []
+    failedLog = []
+    invalidUserLog = []
 
-for x in fullData:
-    if x["status"] == "Accepted password":
-        acceptedLog.append(x)
-    if x["status"] == "Failed Password":
-        failedLog.append(x)
-    if x["status"] == "Invalid User":
-        invalidUserLog.append(x)
+    for x in fullData:
+        if x["status"] == "Accepted password":
+            acceptedLog.append(x)
+        if x["status"] == "Failed Password":
+            failedLog.append(x)
+        if x["status"] == "Invalid User":
+            invalidUserLog.append(x)
 
-counterIP = count_stats(failedLog, 'ip')
-invalidUser = count_stats(invalidUserLog, 'user')
-jamRawan = count_stats(failedLog, 'jam')
+    counterIP = count_stats(failedLog, 'ip')
+    invalidUser = count_stats(invalidUserLog, 'user')
+    jamRawan = count_stats(failedLog, 'jam')
 
-ipAcc = []
-for x in acceptedLog:
-    ipLolos(x, ipAcc)
-ipAcc = set(ipAcc)
+    # Ambil semua IP dari acceptedLog, masukin ke set biar unik
+    ipAcc = {x["ip"] for x in acceptedLog}
 
-for x, y in counterIP.items():
-    if x in ipAcc:
-        print(f"{x} berhasil setelah gagal sebanyak, {y}x")
+    for x, y in counterIP.items():
+        if x in ipAcc:
+            print(f"{x} berhasil setelah gagal sebanyak, {y}x")
 
-ipsort = urutan(counterIP)
-usersort = urutan(invalidUser)
-jamsort = urutan(jamRawan)
+    ipsort = urutan(counterIP)
+    usersort = urutan(invalidUser)
+    jamsort = urutan(jamRawan)
 
-printHasil(ipsort, usersort, jamsort)
+    printHasil(ipsort, usersort, jamsort)
+
+
+if __name__ == "__main__":
+    main()

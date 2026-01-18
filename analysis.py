d4545e5c94bad4ff6746379c28bf893f107b30c7
MRizkiii <rizkigaming048@gmail.com>
2026-01-18 22:57:51 +0700

    add time-based IP tracking logic

--- analysis.py
+++ analysis.py
@@ -9,3 +9,29 @@ def count_stats(data_list, key):
     return tes
 def urutan(isi_dict):
     return sorted(isi_dict.items(), key=lambda x: x[1], reverse=True)
+
+def brute_force_detected(x, data):
+    jam = x["jam"]
+    ip = x["ip"]
+    tgl = x["tanggal"]
+
+    jam = jam.split(":")
+    perhari = int(tgl) * 8760
+    perjam = int(jam[0]) * 3600
+    permenit = int(jam[1]) * 60
+    perdetik = int(jam[2]) * 1
+
+    jam = perhari+perjam + permenit + perdetik
+    
+    kantong = data.get(ip, [])
+    kantong_baru = []
+    for y in kantong:
+        if jam - y <= 60:
+            kantong_baru.append(y)
+
+    kantong_baru.append(jam)
+    data[ip] = kantong_baru
+
+
+    
+    
